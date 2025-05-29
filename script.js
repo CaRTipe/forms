942be2c9eb0c6643e76267df55bc84792bc86828
CaRTipe <blairmbugua868@gmail.com>
2025-05-29 12:31:35 +0300

    Add HTML structure and styling for Rental Management Platform
    - Created houses.html for house selection and tenant dropdown.
    - Developed index.html for user registration with form fields.
    - Implemented signin.html for user sign-in functionality.
    - Added script.js to handle dynamic population of house and tenant options.
    - Introduced style.css for consistent styling across all pages.

---
+++ script.js
@@ -0,0 +1,58 @@
+var house = document.getElementById("house");
+var tenant = document.getElementById("tenant");
+
+function selectHouse() {
+  for (var i = 0; i <= 1000; i++) {
+    var option = document.createElement("option");
+    option.value = i;
+    option.text = "House " + i;
+
+    document.getElementById("house").appendChild(option);
+  }
+}
+selectHouse();
+
+function selectID() {
+  const firstNames = [
+    "John",
+    "Jane",
+    "Michael",
+    "Emily",
+    "Chris",
+    "Jessica",
+    "Daniel",
+    "Ashley",
+    "David",
+    "Sarah",
+  ];
+  const lastNames = [
+    "Smith",
+    "Doe",
+    "Johnson",
+    "Davis",
+    "Brown",
+    "Taylor",
+    "Wilson",
+    "Moore",
+    "Clark",
+    "Lee",
+  ];
+
+  const tenantNames = [];
+
+  for (let i = 0; i < 1000; i++) {
+    const first = firstNames[Math.floor(Math.random() * firstNames.length)];
+    const last = lastNames[Math.floor(Math.random() * lastNames.length)];
+    tenantNames.push(`${first} ${last}`);
+  }
+
+  for (let i=0; i <tenantNames.length; i++) {
+    var option = document.createElement("option");
+    option.value = i;
+    option.text = tenantNames[i];
+
+    document.getElementById("tenant").appendChild(option);
+  }
+  
+}
+selectID()
